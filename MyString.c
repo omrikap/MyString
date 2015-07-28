d1e345de39a42b25158e8af3d06f9b561e09760d
omrikap <omrikap@gmail.com>
2015-07-28 15:37:25 +0300

    Initial commit

---
+++ MyString.c
@@ -0,0 +1,16 @@
+/**
+ * @file MyString.c
+ * @author  Omri Kaplan
+ * @date 28 Jul 2015
+ *
+ * @brief todo
+ *
+ * @section DESCRIPTION
+ * todo
+ */
+// ------------------------------ includes ------------------------------
+#include <stdio.h>
+
+// -------------------------- const definitions -------------------------
+
+// ------------------------------ functions -----------------------------
